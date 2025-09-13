2f6b64b5c7fdf7147004d7077172f9b63bbb254b
Sadeghi Morteza <0660@omidnet.local>
2025-09-13 13:24:27 +0330

    restructure the project to cope with standards

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "parserFactory.hpp"
-#include "structInfo.hpp"
+#include "parser/parserFactory.hpp"
+#include "parser/structInfo.hpp"
 
 using namespace par;
 
@@ -62,28 +62,60 @@ int main(int argc, char *argv[])
     }
     )json";
 
+    std::string cashDeskJson = R"json(
+        {
+            "TerminalCardID": 105632,
+            "ServiceTypeId": 15,
+            "Title": "فروشگاه دهدار",
+            "PAN": "1",
+            "TerminalID": "31317014",
+            "MerchantID": "1",
+            "MerchantName": "تست امید نت",
+            "Description": "...",
+            "IsTwoReceipts": "false",
+            "IsStraightToVAsMenu": false,
+            "TitleEN": "CashDesk",
+            "ServiceType": {
+                "Id": 15,
+                "Title": "CASHDESK",
+                "Description": "صندوقی"
+            },
+            "Details": [
+                {
+                    "TerminalCardID": 105632,
+                    "Name": "فروشگاه دهدار",
+                    "Price": 0,
+                    "Description": "payid=1;title=نام",
+                    "TypeName": "none",
+                    "Code": "000007000772"
+                }
+            ],
+            "RegionID": 168,
+            "WebServiceUrl": null,
+            "EnTerminalGroupTitle": "Buy",
+            "FaTerminalGroupTitle": "خرید",
+            "TerminalGroupDescription": ""
+        }
+     )json";
+
 
     Pattern terminalCardPattern
         {
-            Label{"IsSuccess"},
-            Label{"ErrorMessage"},
-            Map{"Data",false,
+            Iterator{"Details",
+            {
+                List
                 {
-                    Iterator{"Details",
-                    {
-                        List
-                        {
-                            Label{"Name"},
-                            Label{"Price"},
-                            Label{"Description"},
-                            Label{"TypeName"},
-                            Label{"Code"},
-                            Label{"TerminalCardID"}
-                        }
-                    }
+                    Label{"Name"},
+                    Label{"Price"},
+                    Label{"Description"},
+                    Label{"TypeName"},
+                    Label{"Code"},
+                    Label{"TerminalCardID"}
                 }
             }
-        }
+    }
+            
+        
 
     };
 
@@ -110,10 +142,10 @@ int main(int argc, char *argv[])
       {VasMenuAttribute::Counter, "Counter"}
     };
 
-    StructInfo<VasMenuAttribute> structInfo{"./Data/Details/0",attributeNames};
+    StructInfo<VasMenuAttribute> structInfo{"./Details/0",attributeNames};
 
     std::unique_ptr<par::IParser> parser_json = par::create_parser(&terminalCardPattern, InterpretType::JSON);
-    parser_json->parse({structInfo},sampleJSON);
+    parser_json->parse({structInfo},cashDeskJson);
 
     std::cout << structInfo;
 
